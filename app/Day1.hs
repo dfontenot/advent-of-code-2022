1a5953cf0bd7e6afc0a3835487d7aec49da8f4f3
David Fontenot <davidmfontenot@gmail.com>
2022-11-30 22:49:42 -0800

    trying a different approach

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,26 +1,36 @@
 module Main where
 
-import System.Directory
 import Text.Parsec.String (Parser)
 import Text.Parsec.Combinator (many1, skipMany1)
 import Text.ParserCombinators.Parsec hiding (many, State)
 import Text.Parsec.Char (digit, oneOf, char)
+import Control.Monad (void)
 
 type Parsed = [[Integer]]
 
 integer :: Parser Integer
 integer = read <$> many1 digit
 
+integerLine :: Parser Integer
+integerLine = do
+  int_ <- integer
+  void $ char '\n'
+  return int_
+
+calories :: Parser [Integer]
+calories = do
+  lines_ <- manyTill integerLine (try (char '\n'))
+  return lines_
+
 caloriesFile :: Parser Parsed
-caloriesFile = (integer `sepBy` char '\n') `sepBy` char '\n'
+--caloriesFile = calories `sepBy` char '\n'
+caloriesFile = manyTill calories (try (choice [eof, void (char '\n')]))
 
 parseInput :: String -> Either ParseError Parsed
 parseInput = parse caloriesFile "day1-part1.txt" -- 2nd arg is just the filename to use in parseerror s
 
 main :: IO ()
 main = do
-  cwd <- getCurrentDirectory
-  print cwd
   fileInput <- readFile "./data/day1-part1.txt"
   let parsed = parseInput fileInput in
       case parsed of
