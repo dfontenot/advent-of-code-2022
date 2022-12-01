e86cb085a3aeb0ea57ec9d0199840f81cbfda1a1
David Fontenot <davidmfontenot@gmail.com>
2022-11-30 22:20:06 -0800

    need to rework parser

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,5 +1,6 @@
-module Day1 where
+module Main where
 
+import System.Directory
 import Text.Parsec.String (Parser)
 import Text.Parsec.Combinator (many1, skipMany1)
 import Text.ParserCombinators.Parsec hiding (many, State)
@@ -11,16 +12,16 @@ integer :: Parser Integer
 integer = read <$> many1 digit
 
 caloriesFile :: Parser Parsed
-caloriesFile = do
-  calories <- integer `sepBy` char '\n'
-  return (calories:(try ((skipMany1 (char '\n')) >> caloriesFile) <|> (return [])))
+caloriesFile = (integer `sepBy` char '\n') `sepBy` char '\n'
 
 parseInput :: String -> Either ParseError Parsed
 parseInput = parse caloriesFile "day1-part1.txt" -- 2nd arg is just the filename to use in parseerror s
 
 main :: IO ()
 main = do
-  fileInput <- readFile "../data/day1-part1.txt"
+  cwd <- getCurrentDirectory
+  print cwd
+  fileInput <- readFile "./data/day1-part1.txt"
   let parsed = parseInput fileInput in
       case parsed of
         Right result -> print result
