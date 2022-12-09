0226ed8d9fc748be7ca4175400a1e9b6a73cbe01
David Fontenot <davidmfontenot@gmail.com>
2022-12-08 21:42:59 -0800

    incorrect guess for day 8, less than 1990

--- app/Day8.hs
+++ app/Day8.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import qualified Data.Vector as V
+import qualified Data.Set as Set
 import Data.List
 import Control.Monad.State
 
 newtype Matrix a = Matrix (V.Vector (V.Vector a))
+type TreeCoords = (Int, Int)
 
 instance (Show a) => Show (Matrix a) where
   show (Matrix lines_) = "----\n" ++ intercalate "\n" listLines ++ "\n----"
@@ -15,6 +17,10 @@ instance (Show a) => Show (Matrix a) where
 matrixFromList :: (Integral a) => [[a]] -> Matrix a
 matrixFromList lst = Matrix $ V.fromList $ map V.fromList lst
 
+matrixPerimeter :: Matrix a -> Int
+matrixPerimeter (Matrix mat) = let m = V.length mat in
+                                   let n = V.length (mat V.! 0) in m * 2 + n * 2 - 4
+
 -- turn the matrix 90 degrees counter clockwise
 rotateMat :: Matrix a -> Matrix a
 rotateMat (Matrix mat) = Matrix $ collectMat 0 (V.length (mat V.! 0)) mat
@@ -22,25 +28,25 @@ rotateMat (Matrix mat) = Matrix $ collectMat 0 (V.length (mat V.! 0)) mat
     collectMat i len mat' | i <= len - 1 = collectMat (i + 1) len mat' V.++ V.singleton (V.map (V.! i) mat')
     collectMat _ _ _ = V.empty
 
-numTreesInRow :: (Integral a) => V.Vector a -> Int
-numTreesInRow row | V.length row == 0 = 0
-numTreesInRow row = evalState treeScan (0, 0, 0)
+numTreesInRow :: (Integral a) => V.Vector a -> Int -> [TreeCoords]
+numTreesInRow row _ | V.length row == 0 = []
+numTreesInRow row yCoord = evalState treeScan (0, 0, [])
   where
     treeScan = do
-      (pos, heightToBeat, numTreesVisible) <- get
+      (pos, heightToBeat, visibleTrees) <- get
       if pos == V.length row
-         then return numTreesVisible
+         then return visibleTrees
          else let thisTreeHeight = row V.! pos in do
            if thisTreeHeight > heightToBeat
-              then put (pos + 1, thisTreeHeight, numTreesVisible + 1) >> treeScan
-              else put (pos + 1, heightToBeat, numTreesVisible) >> treeScan
+              then put (pos + 1, thisTreeHeight, (pos, yCoord):visibleTrees) >> treeScan
+              else put (pos + 1, heightToBeat, visibleTrees) >> treeScan
 
-treeScanFromLeftSide :: (Integral a) => Matrix a -> Int
+treeScanFromLeftSide :: (Integral a) => Matrix a -> [TreeCoords]
 treeScanFromLeftSide (Matrix forest) = forestScan 1
   where
-    forestScan pos | pos == V.length forest = 0
-    forestScan pos | pos == V.length forest - 1 = 0
-    forestScan pos = numTreesInRow (forest V.! pos) + forestScan (pos + 1)
+    forestScan pos | pos == V.length forest = []
+    forestScan pos | pos == V.length forest - 1 = []
+    forestScan pos = numTreesInRow (forest V.! pos) pos ++ forestScan (pos + 1)
 
 main :: IO ()
 main = do
@@ -48,4 +54,5 @@ main = do
   -- map (map ((read :: String -> Int) . singleton)) (lines input)
   let mat = Matrix $ V.fromList $ map (V.fromList . map ((read :: String -> Int) . singleton)) (lines input) in
       let rotations = [mat, rotateMat mat, (rotateMat . rotateMat) mat, (rotateMat . rotateMat . rotateMat) mat] in -- TODO: ugly
-          print $ sum $ map treeScanFromLeftSide rotations
+          let numUniqueVisibleTrees = length $ Set.fromList $ concatMap treeScanFromLeftSide rotations in
+              print $ numUniqueVisibleTrees + matrixPerimeter mat
