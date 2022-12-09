c8cdfde0276be5ab58c482e9230634b8741d4383
David Fontenot <davidmfontenot@gmail.com>
2022-12-08 20:05:15 -0800

    applies HLS suggestions to old code

--- app/Day8.hs
+++ app/Day8.hs
@@ -9,17 +9,17 @@ newtype Matrix a = Matrix (V.Vector (V.Vector a))
 instance (Show a) => Show (Matrix a) where
   show (Matrix lines_) = "----\n" ++ intercalate "\n" listLines ++ "\n----"
     where
-      listLines = V.toList $ V.map (\line -> intercalate " " (listLine line)) lines_
-      listLine line = if (null line) then ["(empty line)"] else V.toList $ V.map show line
+      listLines = V.toList $ V.map (unwords . listLine) lines_
+      listLine line = if null line then ["(empty line)"] else V.toList $ V.map show line
 
 rotateMat :: Matrix a -> Matrix a
 rotateMat (Matrix mat) = Matrix $ V.fromList $ collectMat 0 (V.length (mat V.! 0)) mat
   where
-    collectMat i len mat' | i <= len - 1 = (V.map (\row -> row V.! i) mat'):(collectMat (i + 1) len mat')
+    collectMat i len mat' | i <= len - 1 = V.map (V.! i) mat':collectMat (i + 1) len mat'
     collectMat _ _ _ = []
 
 main :: IO ()
 main = do
   input <- readFile "./data/day8.txt"
   -- map (map ((read :: String -> Int) . singleton)) (lines input)
-  print $ Matrix $ V.fromList $ map (\line -> V.fromList (map ((read :: String -> Int) . singleton) line)) (lines input)
+  print $ Matrix $ V.fromList $ map (V.fromList . map ((read :: String -> Int) . singleton)) (lines input)
