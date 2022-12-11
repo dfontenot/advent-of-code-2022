bb3dd90c0cf1d54db7af9d412041553cae276b07
David Fontenot <davidmfontenot@gmail.com>
2022-12-11 00:39:02 -0800

    made the state functionality more general

--- app/Day8.hs
+++ app/Day8.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NegativeLiterals #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import qualified Data.Vector as V
@@ -8,29 +9,50 @@ import Control.Monad.State
 
 type GridDimens = (Int, Int)
 type TreeCoords = (Int, Int)
+data TreeScanState = TreeScanState
+  { pos :: Int,
+  heightToBeat :: Int,
+  visibleTrees :: [TreeCoords],
+  isBeginning :: Int -> Int -> Bool,
+  isEnd :: Int -> Int -> Bool,
+  nextTreePos :: Int -> Int
+  }
 
-numTreesInRow :: (Integral a) => V.Vector a -> Int -> [TreeCoords]
-numTreesInRow row _ | V.length row == 0 = []
-numTreesInRow row yCoord = evalState treeScan (0, 0, [])
+numTreesVisibleLeft :: V.Vector Int -> Int -> [TreeCoords]
+numTreesVisibleLeft row _ | V.length row == 0 = []
+numTreesVisibleLeft row yCoord = evalState (numTreesVisible row yCoord) initialState
   where
-    treeScan = do
-      (pos, heightToBeat, visibleTrees) <- get
-      if pos == 0
-         then put (pos + 1, row V.! pos, visibleTrees) >> treeScan
-         else
-          if pos == V.length row
-             then return visibleTrees
-             else let thisTreeHeight = row V.! pos in do
-               if thisTreeHeight > heightToBeat
-                  then put (pos + 1, thisTreeHeight, (pos, yCoord):visibleTrees) >> treeScan
-                  else put (pos + 1, heightToBeat, visibleTrees) >> treeScan
+    initialState = TreeScanState {
+    pos = 0,
+    heightToBeat = 0,
+    visibleTrees = [],
+    isBeginning = \_ pos -> pos == 0,
+    isEnd = \len pos -> pos == len - 1,
+    nextTreePos = (+1)
+  }
 
-treeScanFromLeftSide :: (Integral a) => V.Vector a -> GridDimens -> [TreeCoords]
+numTreesVisible :: V.Vector Int -> Int -> State TreeScanState [TreeCoords]
+numTreesVisible row _ | V.length row == 0 = return []
+numTreesVisible row yCoord = treeScan
+  where
+    treeScan = let len = V.length row in do
+        TreeScanState {..} <- get
+        if isBeginning len pos
+           then put TreeScanState {pos=nextTreePos pos, heightToBeat=row V.! pos, ..} >> treeScan
+           else
+            if isEnd len pos
+               then return visibleTrees
+               else let thisTreeHeight = row V.! pos in do
+                 if thisTreeHeight > heightToBeat
+                    then put TreeScanState {pos=nextTreePos pos, heightToBeat=thisTreeHeight, visibleTrees=(pos, yCoord):visibleTrees, ..} >> treeScan
+                    else put TreeScanState {pos=nextTreePos pos, ..} >> treeScan
+
+treeScanFromLeftSide :: V.Vector Int -> GridDimens -> [TreeCoords]
 treeScanFromLeftSide forest (m, n) = forestScan (1, 0)
   where
     forestScan (_, n') | n' == n = []
     forestScan (_, n') | n' == n - 1 = []
-    forestScan (m', n') = numTreesInRow (V.slice (m' * n') m forest) n' ++ forestScan (0, n' + 1)
+    forestScan (m', n') = numTreesVisibleLeft (V.slice (m' * n') m forest) n' ++ forestScan (0, n' + 1)
 
 main :: IO ()
 main = do
